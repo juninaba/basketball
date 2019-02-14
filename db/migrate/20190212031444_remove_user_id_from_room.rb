6e4c10e2b445613f525e71b20cac917e9086ffb1
Ryosuke Hiroe <ride.poke@gmail.com>
2019-02-14 16:34:00 +0900

    不要なファイルの削除

--- db/migrate/20190212031444_remove_user_id_from_room.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromRoom < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :rooms, :user_id, :integer
-  end
-end
--- db/migrate/20190212031512_remove_user2_id_from_room.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUser2IdFromRoom < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :rooms, :user2_id, :integer
-  end
-end
